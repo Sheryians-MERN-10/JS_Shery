e7bc0c41c1fc11429ff949ef7f729075baa87d10
krayush1109 <hellotoayush@gmail.com>
2024-03-19 18:43:08 +0530

    insta story status - half done

--- _Code-Playground/02_Story_Insta/script.js
+++ _Code-Playground/02_Story_Insta/script.js
@@ -1,16 +1,65 @@
 
-let fun = async () => {
+let instaData = async () => {
     try {
-        let response = await fetch('./data.json')
-        console.log(response);
-        console.log(typeof response);
-        let data = await response.json();
-        console.log(data);
-        console.log(typeof data);
+        // let res = await fetch('./data.json')
+        // console.log(res);
+        // console.log(typeof res);
+        // let data = await res.json();
+
+        // console.log(data);
+        // console.log(data[2]);
+        // console.log(data[0].dp);
+        // console.log(data[0].profile);
+        // console.log(typeof data);
+
+        // ONE LINE SOLUTION
+        return (await fetch('./data.json')).json();
     }
     catch (err) {
-        console.log(err);
-    }   
+        console.error(err);
+    }
 }
 
-fun();
+// instaData();
+instaData()
+    .then((res) => {
+        // console.log(res[2].dp);
+    })
+
+const dp_box = document.querySelector('#dp-box');
+
+instaData()
+    .then((res) => {
+        res.map((elem, i) => {
+            // console.log(elem, i);
+            // console.log(elem.dp)
+            // console.log(elem.profile)
+
+            dp_box.innerHTML +=
+                `<div class="w-20 h-20 rounded-full     overflow-hidden cursor-pointer">
+                 <img src="${elem.dp}" class=" " alt="${i}"
+                 srcset="">
+                 </div>`;
+            
+            return res;
+        })
+    })
+    .then(() => {
+        let dp = document.querySelectorAll('#dp-box img');
+        // console.log(dp)
+        dp.forEach((e, i) => {
+            // console.log(e);
+            // console.log(e.alt);
+            
+            e.addEventListener( 'click', (e) => {
+                console.log(e.target.alt);
+
+            })
+        })
+        
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+
