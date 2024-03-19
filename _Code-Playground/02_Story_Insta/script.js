1bc80358ea503e89aa0130d6f37471084e36068a
krayush1109 <hellotoayush@gmail.com>
2024-03-19 21:35:18 +0530

    Completed with Mobile Responsive

--- _Code-Playground/02_Story_Insta/script.js
+++ _Code-Playground/02_Story_Insta/script.js
@@ -20,46 +20,51 @@ let instaData = async () => {
     }
 }
 
-// instaData();
-instaData()
-    .then((res) => {
-        // console.log(res[2].dp);
-    })
-
 const dp_box = document.querySelector('#dp-box');
 
-instaData()
-    .then((res) => {
-        res.map((elem, i) => {
-            // console.log(elem, i);
-            // console.log(elem.dp)
-            // console.log(elem.profile)
+let handle_dp = async () => {
+    let instaDet = await instaData();
+    console.log(instaDet);
 
-            dp_box.innerHTML +=
-                `<div class="w-20 h-20 rounded-full     overflow-hidden cursor-pointer">
+    instaDet.map((elem, i) => {
+        // console.log(elem, i);
+        // console.log(elem.dp)
+        // console.log(elem.profile)
+        dp_box.innerHTML +=
+            `<div class="w-20 h-20 rounded-full     overflow-hidden cursor-pointer">
                  <img src="${elem.dp}" class=" " alt="${i}"
                  srcset="">
                  </div>`;
-            
-            return res;
-        })
-    })
-    .then(() => {
-        let dp = document.querySelectorAll('#dp-box img');
-        // console.log(dp)
-        dp.forEach((e, i) => {
-            // console.log(e);
-            // console.log(e.alt);
-            
-            e.addEventListener( 'click', (e) => {
-                console.log(e.target.alt);
-
-            })
-        })
-        
-    })
-    .catch((err) => {
-        console.log(err);
     });
 
+    const dp = document.querySelectorAll('#dp-box img');
+    const fullStory = document.querySelector('#storyFull');
+    const timer = document.querySelector('#timer');
+    // console.log(dp)
+    dp.forEach((e, i) => {
+        // console.log(e);
+        // console.log(e.alt);
 
+        e.addEventListener('click', (e) => {
+            console.log(e.target.alt);
+            console.log(instaDet[0].profile)
+            console.log(instaDet[e.target.alt].profile)
+        
+            let imgLink = instaDet[e.target.alt].profile;
+            fullStory.innerHTML = 
+                `
+                <img src="${imgLink}" class="w-full h-full object-cover" alt="" srcset="">
+                <div id="timer" class="border-yellow-400 opacity-80 border-t-8 absolute top-0 w-full"></div>
+                `;
+
+            
+            fullStory.style.display = 'initial';            
+            setTimeout(() => {
+                fullStory.style.display = 'none';
+            }, 1500);
+        })
+    })
+
+
+}
+handle_dp();
